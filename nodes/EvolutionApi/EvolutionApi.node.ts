20add38fbeb15be16c385cede2129bb892b39682
ujang19 <34524528+ujang19@users.noreply.github.com>
2025-07-24 12:50:56 +0700

    Update EvolutionApi.node.ts

--- nodes/EvolutionApi/EvolutionApi.node.ts
+++ nodes/EvolutionApi/EvolutionApi.node.ts
@@ -10,7 +10,7 @@ import { resourceOperationsFunctions } from './execute';
 
 export class EvolutionApi implements INodeType {
 	description: INodeTypeDescription = {
-               displayName: 'Zapin EVO',
+               displayName: 'Zapin EVO API',
 		name: 'evolutionApi',
 		icon: 'file:evolutionapi.svg',
 		group: ['transform'],
